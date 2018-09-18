82c493372544cfbb3ce98ab8b0d4229677873aab
Elliott Partridge <elliott.partridge@gmail.com>
2018-09-18 16:01:49 -0400

    Update serlog.sh
    Added comments

--- usr/bin/serlog.sh
+++ usr/bin/serlog.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Maybe add some input sanitization later...
+
+# Configure the serial port settings (115.2 kbaud)
 stty -F /dev/$1 115200 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke
+# Print serial port input on stdout, removing null characters (which screw up journalctl)
 tr < /dev/$1 -d '\000'
